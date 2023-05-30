e27f2a8cc0d910b72743228b80933e62f12e4fb4
routhushiva <routhushiva779>
2023-05-30 19:21:11 +0530

    Modified Demo.java

--- src/package1/Demo.java
+++ src/package1/Demo.java
@@ -4,12 +4,14 @@ public class Demo
 {
 	public static void main(String[] args)
 	{
-		String str1 = "9990";
+		String str1 = "9100";
 		String str2 = "90";
 		int a = Integer.parseInt(str1);
 		int b = Integer.parseInt(str2);
 		System.out.println(a+b);
 		System.out.println(a/b);
+		System.out.println(a-b);
+		System.out.println(a*b);
 		
 		int x = 66;
 		int y = 77;
@@ -17,5 +19,6 @@ public class Demo
 		String st2 = String.valueOf(y);
 		System.out.println(st1+st2);
 		System.out.println(st2+st1);
+		
 	}
 }
